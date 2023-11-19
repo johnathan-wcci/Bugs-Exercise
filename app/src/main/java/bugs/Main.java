611bfb15a20ac2358f40edf0504a3ed850f6cbe1
Johnathan Alexander <128754235+johnathan-wcci@users.noreply.github.com>
2023-11-19 04:44:26 -0500

    Initial Commit

---
+++ app/src/main/java/bugs/Main.java
@@ -0,0 +1,8 @@
+package bugs;
+
+public class Main {
+    public static void main(String[] args) {
+        App OurApp = new App();
+        System.out.println(OurApp.repeatSeparator("Word", "X", 3));
+    }
+}
